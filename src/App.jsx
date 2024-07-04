641b28b36e86c641bcc9dccbaadbc42ef5c238ca
catarina-bsilva <work@catarinasilva.com>
2024-07-04 23:43:44 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { addClipboardEventListener, removeClipboardEventListener } from './components/clipboard'
-import { useEffect } from "react"
+  import { addClipboardEventListener, removeClipboardEventListener } from './components/clipboard'
+  import { useEffect } from "react"
 
-function App() {
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = "/macklib.zip"
-    link.download = 'macklib.zip';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  useEffect(() => {
-    addClipboardEventListener();
-
-    return () => {
-      removeClipboardEventListener();
+  function App() {
+    const handleDownload = () => {
+      const link = document.createElement('a');
+      link.href = "/macklib.zip"
+      link.download = 'macklib.zip';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     };
-  }, []);
 
-  return (
-    <div className="container">
-      <button style={{marginTop: '30vh', padding:'25px'}}className='btn-lg btn-color3 fontSize-md' onClick={handleDownload}>Download library Macklib</button>
-    </div>
-  )
-}
+    useEffect(() => {
+      addClipboardEventListener();
 
-export default App
+      return () => {
+        removeClipboardEventListener();
+      };
+    }, []);
+
+    return (
+      <div className="container">
+        <button style={{marginTop: '30vh', padding:'25px'}}className='btn-lg btn-color3 fontSize-md' onClick={handleDownload}>Download library Macklib</button>
+      </div>
+    )
+  }
+
+  export default App
